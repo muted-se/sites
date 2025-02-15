ca35ad8a87a43f4bb70f273d92e87e002246d5cd
Joakim Dellrud <joakim@dellrud.se>
2025-02-15 12:31:02 +0100

    Cleanup and simplify cd

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
 function _wrapper() {
-    podman run --publish 8000:8000 --entrypoint /bin/bash --tty --interactive --rm --workdir /app --volume $(pwd):/app:rw --volume $(pwd)/src:/src:ro python:3.11.9-bookworm "${@}"
+    podman run --publish 8000:8000 --entrypoint /bin/bash --tty --interactive --rm --workdir /app --volume $(pwd):/app:Z --volume $(pwd)/src:/src:ro python:3.11.9-bookworm "${@}"
 }
 _wrapper "${@}"
